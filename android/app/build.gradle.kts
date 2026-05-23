61c4d7603fdb7b0d733d078881ff29c621789781
Carlos Eduardo <carlosedu2411@gmail.com>
2026-05-23 09:20:06 -0300

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app_rotina_mvp"
+    namespace = "com.heroi.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app_rotina_mvp"
+        applicationId = "com.heroi.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -29,12 +29,19 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+signingConfigs {
+        create("release") {
+            keyAlias = "upload"
+            keyPassword = "rotinadoheroi"
+            storeFile = file("C:\\Users\\Carlos\\upload-keystore.jks")
+            storePassword = "rotinadoheroi"
+        }
+    }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
