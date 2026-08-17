f9f7225e5eeb9e384de7dd4eecaac6c8ea488fbd
Harry Rocha Taveiros <harry.rt2006@gmail.com>
2026-08-16 23:00:24 -0300

    Ajuste na versão do Java/Kotlin para 17 e ativação do desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,8 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
+   compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -48,4 +49,8 @@ signingConfigs {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
